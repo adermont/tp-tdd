d4ee27d4b39575eae5f107278178e290b9b5d02f
adermont <adermont.github@gmail.com>
2023-05-08 16:38:03 +0200

    Exercice 2 - Test 2 : implementation

--- src/main/java/fr/simplon/tdd/ex2/CompteBancaire.java
+++ src/main/java/fr/simplon/tdd/ex2/CompteBancaire.java
@@ -2,11 +2,19 @@ package fr.simplon.tdd.ex2;
 
 public class CompteBancaire
 {
+    private int mSolde;
+
     public CompteBancaire(int solde)
     {
         if (solde <= 0)
         {
             throw new IllegalArgumentException("Le solde doit être obligatoirement positif");
         }
+        mSolde = solde;
+    }
+
+    public int getSolde()
+    {
+        return mSolde;
     }
 }
