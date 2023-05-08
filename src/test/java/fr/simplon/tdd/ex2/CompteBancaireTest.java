5dd0179c0ec438191136b74326ffae00c17aab69
adermont <adermont.github@gmail.com>
2023-05-08 16:34:04 +0200

    Exercice 2 - Test 2 : noimpl

--- src/test/java/fr/simplon/tdd/ex2/CompteBancaireTest.java
+++ src/test/java/fr/simplon/tdd/ex2/CompteBancaireTest.java
@@ -3,6 +3,8 @@ package fr.simplon.tdd.ex2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 class CompteBancaireTest
 {
     /**
@@ -14,5 +16,19 @@ class CompteBancaireTest
         Assertions.assertThrows(IllegalArgumentException.class, () -> new CompteBancaire(0));
         Assertions.assertThrows(IllegalArgumentException.class, () -> new CompteBancaire(-1));
         Assertions.assertDoesNotThrow(() -> new CompteBancaire(1));
+    }
+
+    /**
+     * Test 2: On peut consulter le solde d'un compte. Après la création du compte, le solde est identique à la somme
+     * versée lors de l'ouverture.
+     */
+    @Test
+    public void testConsulterSolde()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            int solde = new Random().nextInt(1, Integer.MAX_VALUE);
+            Assertions.assertEquals(solde, new CompteBancaire(solde).getSolde());
+        }
     }
 }
